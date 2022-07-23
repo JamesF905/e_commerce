d5e10f8c3c350151595c4fa76899ba14434870ac
James <jameslfidlin@gmail.com>
2022-07-23 12:26:11 -0400

    category model done

--- models/Category.js
+++ models/Category.js
@@ -7,6 +7,31 @@ class Category extends Model {}
 Category.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+      allowNull: false
+    },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    author: {
+      type: DataTypes.STRING
+    },
+    isbn: {
+      type: DataTypes.STRING
+    },
+    pages: {
+      type: DataTypes.INTEGER
+    },
+    edition: {
+      type: DataTypes.INTEGER
+    },
+    is_paperback: {
+      type: DataTypes.BOOLEAN
+    }
     /*
     `id`
 
