1c036b836dfd49ba0e04437a359f2d7bbf0916fa
James <jameslfidlin@gmail.com>
2022-07-23 15:09:18 -0400

    finished converting category route functions, removed the part of the inclues where it pulled the associated product data through the through

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -6,28 +6,28 @@ const { Category, Product } = require('../../models');
 router.get('/', (req, res) => {
   // find all categories
   // be sure to include its associated Products
-  Category.findALL({include: [{ model: Product, through: ProductTag}]})
-  .then((tag) => res.json(tag))
+  Category.findALL({include: [{ model: Product}]})
+  .then((cats) => res.json(cats))
   .catch((err) => res.status(500).json(err));
 });
 
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
-  Tag.findByPk(req.params.id, {include: [{ model: Product, through: ProductTag}]})
-      .then((tag) =>
-        !tag
-          ? res.status(404).json({ message: 'No tag with that ID' })
-          : res.json(tag)
+  Category.findByPk(req.params.id, {include: [{ model: Product}]})
+      .then((cats) =>
+        !cats
+          ? res.status(404).json({ message: 'No category with that ID' })
+          : res.json(cats)
       )
       .catch((err) => res.status(500).json(err));
 });
 
 router.post('/', (req, res) => {
   // create a new category
-  Tag.create(req.body)
-    .then((tag) => {
-      res.status(200).json(tag);
+  Category.create(req.body)
+    .then((cats) => {
+      res.status(200).json(cats);
     })
     .catch((err) => {
       res.status(400).json(err);
@@ -36,22 +36,22 @@ router.post('/', (req, res) => {
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
-  Tag.update(req.body, {where: {id: req.params.id}})
-  .then((tag) =>
-    !tag
-      ? res.status(404).json({ message: 'No tag with this id!' })
-      : res.json(tag)
+  Category.update(req.body, {where: {id: req.params.id}})
+  .then((cats) =>
+    !cats
+      ? res.status(404).json({ message: 'No category with this id!' })
+      : res.json(cats)
   )
   .catch((err) => res.status(500).json(err));
 });
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
-  Tag.destroy({where: {id: req.params.id}})
-    .then((tag) =>
-      !tag
-        ? res.status(404).json({ message: 'No tag with this id!' })
-        : res.json(tag)
+  Category.destroy({where: {id: req.params.id}})
+    .then((cats) =>
+      !cats
+        ? res.status(404).json({ message: 'No catrgory with this id!' })
+        : res.json(cats)
     )
     .catch((err) => res.status(500).json(err));
 });
