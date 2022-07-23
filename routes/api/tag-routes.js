7bec9159e1ec54323260beb8f7c147bc0fa3e326
James <jameslfidlin@gmail.com>
2022-07-23 13:53:01 -0400

    tag route upsates, convert try to promises

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -36,10 +36,38 @@ router.post('/', (req, res) => {
 
 router.put('/:id', (req, res) => {
   // update a tag's name by its `id` value
+  Tag.findOneAndUpdate(
+    { _id: req.params.thoughtId },
+    { $set: req.body },
+    { runValidators: true, new: true }
+  )
+    .then((tag) =>
+      !tag
+        ? res.status(404).json({ message: 'No tag with this id!' })
+        : res.json(tag)
+    )
+    .catch((err) => res.status(500).json(err));
 });
 
 router.delete('/:id', (req, res) => {
   // delete on tag by its `id` value
+    try {
+      const locationData = await Location.destroy({
+        where: {
+          id: req.params.id
+        }
+      });
+  
+      if (!locationData) {
+        res.status(404).json({ message: 'No location found with this id!' });
+        return;
+      }
+  
+      res.status(200).json(locationData);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  
 });
 
 module.exports = router;
