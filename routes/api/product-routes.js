551e195ae1e8c2d542f56915da4c3d8e6cb74c93
James <jameslfidlin@gmail.com>
2022-07-23 14:49:54 -0400

    beginning the product-routes get routes functions

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -7,12 +7,22 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+  Tag.findALL({include: [{ model: Product, through: ProductTag}]})
+  .then((tag) => res.json(tag))
+  .catch((err) => res.status(500).json(err));
 });
 
 // get one product
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  Tag.findByPk(req.params.id, {include: [{ model: Product, through: ProductTag}]})
+      .then((tag) =>
+        !tag
+          ? res.status(404).json({ message: 'No tag with that ID' })
+          : res.json(tag)
+      )
+      .catch((err) => res.status(500).json(err));
 });
 
 // create new product
